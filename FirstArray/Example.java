e43f496d342b499f7b5d66a55ec4751e920a7ba6
NickHeinzinger <nicholas.heinzinger@vtsd.com>
2022-12-20 09:57:27 -0500

    Day 2

--- FirstArray/Example.java
+++ FirstArray/Example.java
@@ -4,12 +4,18 @@ public class Example {
     public static void main(String args[]){
         //Arrays store one data type
         int numberOfChristmasGifts[];
-        numberOfChristmasGifts = new int[2];
+        numberOfChristmasGifts = new int[200];
 
-        numberOfChristmasGifts[0] = 7;
-        numberOfChristmasGifts[1] = 3;
+        for (int n = 0; n<numberOfChristmasGifts.length; n++){
+            numberOfChristmasGifts[n] = 6;
+            System.out.println(numberOfChristmasGifts[n]);
+        }
 
-        System.out.println(numberOfChristmasGifts[0]);
+
+       // numberOfChristmasGifts[0] = 7;
+        // numberOfChristmasGifts[1] = 3;
+
+       // System.out.println(numberOfChristmasGifts[0]);
 
     }
 }
